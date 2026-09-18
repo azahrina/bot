7f23d3b933f7a4df3763aa58f33160eac64542be
azahrina <bottololocypus@gmail.com>
2026-09-18 10:44:30 +0700

    Delete ig directory

--- ig/content/inject.js
+++
@@ -1,5 +0,0 @@
-// content/inject.js - DEPRECATED
-// Logika injeksi identitas telah dipindahkan ke background.js 
-// menggunakan metode chrome.scripting.executeScript (Native Injection)
-// untuk menghindari error Content Security Policy (CSP).
-console.log("[BOT TOLOL] Legacy Injector: Native Mode Active.");
